6e890a6d723942aec967b5e026ad64e6bd070ffd
vansh-commits <vansh.malhotra439@hotmail.com>
2025-04-24 21:13:26 +0530

    added required routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('/api/v1/user/signup')
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('/api/v1/user/signin')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('/api/v1/blog')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('/api/v1/blog')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('/api/v1/blog/:id')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('/api/v1/blog/bulk')
 })
 
 export default app
