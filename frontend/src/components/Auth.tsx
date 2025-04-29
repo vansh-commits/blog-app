b3bc789f5622544964bb57fe87340de4ad3bc015
vansh-commits <vansh.malhotra439@hotmail.com>
2025-04-29 23:54:55 +0530

    fixed the blog url

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -16,8 +16,8 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
         try {
             const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs);
             const jwt = response.data;
-            localStorage.setItem("token", jwt);
-            navigate("/blogs");
+            localStorage.setItem("token", jwt.jwt);
+            navigate("/blog");
         } catch(e) {
             alert("Error while signing up")
             // alert the user here that the request failed
@@ -74,6 +74,6 @@ interface LabelledInputType {
 function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
     return <div>
         <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-        <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+        <input onChange={onChange} type={type || "text"} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
     </div>
 }
