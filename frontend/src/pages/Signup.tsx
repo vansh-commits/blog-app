1fcdc1ad5a6b7cab3781fb3877f51432045d0e91
vansh-commits <vansh.malhotra439@hotmail.com>
2025-04-29 22:49:59 +0530

    made signup page

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Quote from '../components/Quote'
+import { Auth } from '../components/Auth'
 
 const Signup = () => {
-  return (
-    <div>
-      signup
-    </div>
-  )
+    return (
+        <div>
+            <div className="grid lg:grid-cols-2 grid-cols-1">
+                <div>
+                    <Auth type="signup" />
+                </div>
+                <div className="hidden lg:block">
+                    <Quote />
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default Signup
