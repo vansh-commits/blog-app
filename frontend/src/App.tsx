9e6670746f3175948e3d26ffc8262a686a372433
vansh-commits <vansh.malhotra439@hotmail.com>
2025-04-29 13:53:04 +0530

    initialised a frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./index.css"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Blog from "./pages/Blog"
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
