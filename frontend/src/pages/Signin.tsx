69dac03dafcb2c5967fd415c7cc90ddd2cfdc338
vansh-commits <vansh.malhotra439@hotmail.com>
2025-04-29 22:59:51 +0530

    made signin page

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import Quote from '../components/Quote'
+import { Auth } from '../components/Auth'
 
 const Signin = () => {
+  
   return (
     <div>
-      signin
+        <div className="grid lg:grid-cols-2 grid-cols-1">
+            <div>
+                <Auth type="signin" />
+            </div>
+            <div className="hidden lg:block">
+                <Quote />
+            </div>
+        </div>
     </div>
-  )
+
+)
+  
 }
 
 export default Signin
