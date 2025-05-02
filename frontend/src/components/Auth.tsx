2938dcd1fac637f47acdb605efd674ea9ea4392c
vansh-commits <vansh.malhotra439@hotmail.com>
2025-05-03 00:36:57 +0530

    refractors

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -39,13 +39,13 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                     </div>
                 </div>
                 <div className="pt-8">
-                    {type === "signup" ? <LabelledInput label="Name" placeholder="Harkirat Singh..." onChange={(e) => {
+                    {type === "signup" ? <LabelledInput label="Name" placeholder="Jhon Doe..." onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
                             name: e.target.value
                         })
                     }} /> : null}
-                    <LabelledInput label="Username" placeholder="harkirat@gmail.com" onChange={(e) => {
+                    <LabelledInput label="Username" placeholder="doe@gmail.com" onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
                             email: e.target.value
