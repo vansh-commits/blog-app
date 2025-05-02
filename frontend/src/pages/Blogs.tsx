4355a6135de865353b0c645f63a5421cc82e17e2
vansh-commits <vansh.malhotra439@hotmail.com>
2025-05-02 23:50:03 +0530

    made the all blogs pages

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,42 @@
+import { Appbar } from "../components/Appbar"
+import { BlogCard } from "../components/BlogCard"
+import { BlogSkeleton } from "../components/BlogSkeleton";
+import { useBlogs } from "../hooks";
+
+const Blogs = () => {
+    const { loading, blogs } = useBlogs();
+
+    if (loading) {
+        return <div>
+            <Appbar /> 
+            <div  className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                </div>
+            </div>
+        </div>
+    }
+
+    return <div>
+        <Appbar />
+        <div  className="flex justify-center">
+            <div>
+                {blogs.map((blog, index) => <BlogCard
+                    key={index}
+                    id={blog.id}
+                    authorName={blog.author.name || "Anonymous"}
+                    title={blog.title}
+                    content={blog.content}
+                    publishedDate={"2nd Feb 2024"}
+                />)}
+            </div>
+        </div>
+    </div>
+}
+
+export default Blogs
+
