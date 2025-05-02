339a5ea894551d4f005d26db99f673b51c599822
vansh-commits <vansh.malhotra439@hotmail.com>
2025-05-02 23:55:08 +0530

    made all the routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import "./index.css"
-import Signup from "./pages/Signup"
-import Signin from "./pages/Signin"
-import Blog from "./pages/Blog"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup';
+import Signin  from './pages/Signin'
+import  Blog  from './pages/Blog'
+import Blogs from './pages/Blogs';
+import  Publish  from './pages/Publish';
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-    </BrowserRouter>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Signup/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
